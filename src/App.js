b373cb527de57a697c9e547c2d968c8b679109c8
Antônio Campos <aclcampos@live.com>
2021-10-23 20:17:06 -0300

    Realizado requisito 2

--- src/App.js
+++ src/App.js
@@ -9,10 +9,52 @@ import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.btnEnable = this.btnEnable.bind(this);
+
+    this.state = {
+      loginName: '',
+      loginButtonDisabled: true,
+    };
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => this.btnEnable());
+  }
+
+  btnEnable() {
+    const { loginName } = this.state;
+    const minimumLoginSize = 3;
+    if (loginName.length >= minimumLoginSize) {
+      this.setState({
+        loginButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        loginButtonDisabled: true,
+      });
+    }
+  }
+
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ Login } />
+        <Route
+          path="/"
+          render={ (props) => (<Login
+            { ...props }
+            { ...this.state }
+            btnEnable={ this.btnEnable }
+            onInputChange={ this.onInputChange }
+          />) }
+        />
         <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
         <Route path="/favorites" component={ Favorites } />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,93 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Router, Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.loginSubmit = this.loginSubmit.bind(this);
+    this.redirectSearch = this.redirectSearch.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+
+    this.state = {
+      loading: false,
+      redirectSearch: false,
+    };
+  }
+
+  redirectSearch() {
+    const { loading } = this.state;
+    console.log('oi');
+    return (
+      <Router>
+        {loading ? null : <Redirect to="/search" />}
+      </Router>
+    );
+  }
+
+  async loginSubmit(event) {
+    event.preventDefault();
+    const { loginName } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name: loginName });
+    this.setState({
+      loading: false,
+      redirectSearch: true,
+    });
+  }
+
+  renderForm() {
+    const { loginButtonDisabled, onInputChange, loginName } = this.props;
+    const { loading } = this.state;
+    if (loading) {
+      return <p>Carregando...</p>;
+    }
+    return (
+      <form
+        onSubmit={ this.loginSubmit }
+      >
+        <label htmlFor="name">
+          Login
+          <input
+            id="name"
+            name="loginName"
+            value={ loginName }
+            onChange={ onInputChange }
+            type="text"
+            data-testid="login-name-input"
+          />
+        </label>
+
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ loginButtonDisabled }
+        >
+          Entrar
+        </button>
+      </form>
+    );
+  }
+
   render() {
+    const { redirectSearch } = this.state;
+
     return (
       <div data-testid="page-login">
-        Login
+        {
+          redirectSearch ? <Redirect to="/search" /> : this.renderForm()
+        }
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  loginButtonDisabled: PropTypes.bool.isRequired,
+  loginName: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
 
 export default Login;
