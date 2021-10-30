64ace1988dfec5bf69fe18091c544dfa5bc3aa8b
Antônio Campos <aclcampos@live.com>
2021-10-30 13:48:14 -0300

    Realizado requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+    };
+
+    this.getUserData = this.getUserData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  async getUserData() {
+    this.setState({ loading: true });
+    const userData = await getUser();
+    this.setState({
+      loading: false,
+      name: userData.name,
+      email: userData.email,
+      image: userData.image,
+      description: userData.description,
+    });
+  }
+
   render() {
+    const { loading, name, email, image, description } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {
+          loading ? <p>Carregando...</p> : (
+            <div>
+              <img src={ image } alt="" data-testid="profile-image" />
+              <p>{name}</p>
+              <p>{email}</p>
+              <p>{description}</p>
+              <Link
+                to="/profile/edit"
+              >
+                {' '}
+                Editar perfil
+                {' '}
+
+              </Link>
+            </div>
+          )
+        }
       </div>
     );
   }
